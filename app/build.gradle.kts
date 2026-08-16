7856e94d1587676dce53ca60440c961127524042
halla-dev <halla-dev@users.noreply.github.com>
2026-08-16 23:38:33 +0000

    build: use Android-compatible Bouncy Castle artifact

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,6 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("org.bouncycastle:bcprov-jdk18on:1.82")
+    implementation("org.bouncycastle:bcprov-jdk15to18:1.82")
     testImplementation("junit:junit:4.13.2")
 }
